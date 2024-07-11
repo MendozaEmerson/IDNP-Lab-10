2f58e6ef928840c491896808074caa387575e9ea
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-07-10 19:59:22 -0500

    first commit

---
+++ app/src/main/java/com/example/background/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.background
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        val btnStop = findViewById<Button>(R.id.btnStop)
+
+        btnPlay.setOnClickListener(onClickListenerPlay())
+
+    }
+
+    private fun onClickListenerPlay():(View)->Unit={
+        val audioPlayServiceIntent = Intent(applicationContext, AudioPlayServices::class.java)
+        audioPlayServiceIntent.putExtra(AudioPlayServices.FILENAME, "mariposa.mp3")
+        audioPlayServiceIntent.putExtra(AudioPlayServices.COMMAND, AudioPlayServices.PLAY)
+        startService(audioPlayServiceIntent)
+    }
+}
